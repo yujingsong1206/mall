18deb35dfff5a0ce0f10fb6e92ccc891367284df
yujingsong1206 <646783598@qq.com>
2017-11-27 10:11:09 +0800

    增加校验是否是管理员功能

--- src/main/java/com/mall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mall/service/impl/UserServiceImpl.java
@@ -186,4 +186,17 @@ public class UserServiceImpl implements IUserService {
         return ServerResponse.createBySuccess(user);
     }
 
+    /**
+     * 校验是否是管理员
+     * @param user
+     * @return
+     */
+    @Override
+    public ServerResponse checkAdminRole(User user) {
+        if(user != null && user.getRole().intValue() == Const.Role.ROLE_ADMIN) {
+            return ServerResponse.createBySuccess();
+        }
+        return ServerResponse.createByError();
+    }
+
 }
