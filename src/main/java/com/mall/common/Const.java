9d6466718a2420d911b622b69520dab8f9ed7484
yujingsong1206 <646783598@qq.com>
2017-11-28 17:18:45 +0800

    增加购物车状态常量

--- src/main/java/com/mall/common/Const.java
+++ src/main/java/com/mall/common/Const.java
@@ -23,6 +23,13 @@ public class Const {
         Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_asc", "price_desc");
     }
 
+    public interface Cart {
+        int CHECKED = 1;//购物车选中状态
+        int UN_CHECKED = 0;//购物车未选择状态
+        String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
+        String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
+    }
+
     public enum ProductStatusEnum {
         ON_SALE(1, "在线");
 
