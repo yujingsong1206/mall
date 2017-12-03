6e242b6c9adc65160bd7e9e9ae907685c4daab19
yujingsong1206 <646783598@qq.com>
2017-12-03 16:25:04 +0800

    增加收货地址vo，用于前端数据显示

---
+++ src/main/java/com/mall/vo/ShippingVo.java
@@ -0,0 +1,87 @@
+package com.mall.vo;
+
+/**
+ * Created by song-pc on 2017/12/3.
+ */
+public class ShippingVo {
+
+    private String receiverName;//收货姓名
+
+    private String receiverPhone;//收货固定电话
+
+    private String receiverMobile;//收货移动电话
+
+    private String receiverProvince;//省份
+
+    private String receiverCity;//城市
+
+    private String receiverDistrict;//区/县
+
+    private String receiverAddress;//详细地址
+
+    private String receiverZip;//邮编
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getReceiverPhone() {
+        return receiverPhone;
+    }
+
+    public void setReceiverPhone(String receiverPhone) {
+        this.receiverPhone = receiverPhone;
+    }
+
+    public String getReceiverMobile() {
+        return receiverMobile;
+    }
+
+    public void setReceiverMobile(String receiverMobile) {
+        this.receiverMobile = receiverMobile;
+    }
+
+    public String getReceiverProvince() {
+        return receiverProvince;
+    }
+
+    public void setReceiverProvince(String receiverProvince) {
+        this.receiverProvince = receiverProvince;
+    }
+
+    public String getReceiverCity() {
+        return receiverCity;
+    }
+
+    public void setReceiverCity(String receiverCity) {
+        this.receiverCity = receiverCity;
+    }
+
+    public String getReceiverDistrict() {
+        return receiverDistrict;
+    }
+
+    public void setReceiverDistrict(String receiverDistrict) {
+        this.receiverDistrict = receiverDistrict;
+    }
+
+    public String getReceiverAddress() {
+        return receiverAddress;
+    }
+
+    public void setReceiverAddress(String receiverAddress) {
+        this.receiverAddress = receiverAddress;
+    }
+
+    public String getReceiverZip() {
+        return receiverZip;
+    }
+
+    public void setReceiverZip(String receiverZip) {
+        this.receiverZip = receiverZip;
+    }
+}
