63484dec766f8c08c3eaf2ae6fe8d115d615ca21
yujingsong1206 <646783598@qq.com>
2018-12-25 09:32:24 +0800

    'logger'

--- src/main/java/com/mall/task/CloseOrderTask.java
+++ src/main/java/com/mall/task/CloseOrderTask.java
@@ -58,7 +58,7 @@ public class CloseOrderTask {
 
     @Scheduled(cron = "0 */1 * * * ?")//每1分钟（每个1分钟的整数倍）
     public void closeOrderTaskV3(){
-        logger.info("关闭订单定时任务启动");
+//        logger.info("关闭订单定时任务启动");
         long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout","5000"));
         Long setnxResult = RedisShardedPoolUtil.setnx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis()+lockTimeout));
         if(setnxResult != null && setnxResult.intValue() == 1){
@@ -80,7 +80,7 @@ public class CloseOrderTask {
                 logger.info("没有获得分布式锁：{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
             }
         }
-        logger.info("关闭订单定时任务结束");
+//        logger.info("关闭订单定时任务结束");
     }
 
 //    public void closeOrderTaskV4(){
@@ -108,11 +108,11 @@ public class CloseOrderTask {
 
     private void closeOrder(String lockName){
         RedisShardedPoolUtil.expire(lockName, 5);//有效期5s，防止死锁
-        logger.info("获取{},ThreadName:{}", lockName, Thread.currentThread().getName());
+//        logger.info("获取{},ThreadName:{}", lockName, Thread.currentThread().getName());
         int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
         iOrderService.closeOrder(hour);
         RedisShardedPoolUtil.del(lockName);
-        logger.info("释放{},ThreadName:{}", lockName, Thread.currentThread().getName());
+//        logger.info("释放{},ThreadName:{}", lockName, Thread.currentThread().getName());
     }
 
 }
